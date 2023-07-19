74ae03bf58c8db0cb59de8c72d2e2b831725a062
César <cesarlpb@gmail.com>
2023-07-19 17:02:59 +0200

    Sequelize - tablas Posts y Sections

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const Sequelize = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(
  process.env.DB_NAME,
  process.env.DB_USER,
@@ -10,8 +10,45 @@ const sequelize = new Sequelize(
   }
 );
 
-sequelize.authenticate().then(() => {
-  console.log('Hemos conectado a la base de datos.');
+const Post = sequelize.define("posts", {
+  title: {
+    type: DataTypes.STRING(75),
+    allowNull: false, 
+  },
+  subtitle: {
+    type: DataTypes.STRING(150),
+    allowNull: true
+  },
+  img: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  sectionId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
+
+const Section = sequelize.define("sections", {
+   section: {
+     type: DataTypes.STRING(100),
+     allowNull: false
+   },
+   order: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: 0
+   }
+});
+
+// sequelize.authenticate().then(() => {
+//   console.log('Hemos conectado a la base de datos.');
+// }).catch((error) => {
+//   console.error('Ha habido un error: ', error);
+// });
+
+sequelize.sync().then(() => {
+  console.log('Tablas creadas');
 }).catch((error) => {
-  console.error('Ha habido un error: ', error);
+  console.error('Hubo un error: ', error);
 });

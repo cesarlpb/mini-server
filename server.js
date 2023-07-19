a8cd4c0c2e3a48ba20d8be4fe4039abdac2a764a
César <cesarlpb@gmail.com>
2023-07-19 20:25:47 +0200

    Sequelize - create, bulkCreate y destroy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
-const { Sequelize, DataTypes } = require("sequelize");
+const { truncate } = require('fs');
+const { Op, Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(
  process.env.DB_NAME,
  process.env.DB_USER,
@@ -29,12 +30,75 @@ const Post = sequelize.define("posts", {
   }
 });
 
-// Post de ejemplo
-const nuevoPost = {
-  title: "Viajes a mitad de precio", 
-  subtitle: "Aprende como ahorrar dinero este verano", 
-  img: "playa.jpg", 
-  sectionId: 1}
+// Array de posts
+const posts = [
+  {
+    title: "Viaje a Roma",
+    subtitle: "Pizzas al mejor precio",
+    img: "roma.png",
+    sectionId: 1
+  },
+  {
+    title: "Viaje a Toledo",
+    subtitle: "Toletum y buen acero",
+    img: "toledo.jpg",
+    sectionId: 1
+  },
+  {
+    title: "Viaje a Salamanca",
+    subtitle: "Hay ranas",
+    img: "salamanca.jpg",
+    sectionId: 1
+  },
+  {
+    title: "Viaje a Uganda",
+    subtitle: "Hay gente y no nieva (por ahora)",
+    img: "uganda-forever.jpg",
+    sectionId: 1  
+  },
+  {
+    title: "Roma 2",
+    subtitle: "Pizzas 2",
+    img: "roma.png",
+    sectionId: 2
+  },
+  {
+    title: "Toledo 2",
+    subtitle: "Toletum",
+    img: "toledo.jpg",
+    sectionId: 2
+  },
+  {
+    title: "Salamanca 2",
+    subtitle: "",
+    img: "salamanca.jpg",
+    sectionId: 2
+  },
+  {
+    title: "Uganda 2",
+    subtitle: "",
+    img: "uganda-forever.jpg" ,
+    sectionId: 2
+  },
+  {
+    title: "10 tips para ir a la playa",
+    subtitle: "Playa playa playa",
+    img: "playa.jpg",
+    sectionId: 3
+  },
+  {
+    title: "10 trucos para hacer la maleta",
+    subtitle: "",
+    img: "maleta.jpg",
+    sectionId: 3
+  },
+  {
+    title: "3 tips para viajar en coche",
+    subtitle: "No te dejes el bocadillo",
+    img: "car.jpg",
+    sectionId: 3
+  }
+]
 
 const Section = sequelize.define("sections", {
    section: {
@@ -49,17 +113,33 @@ const Section = sequelize.define("sections", {
 });
 
 // Section de ejemplo
-const nuevaSection = {
-  section: "Viajes exprés en el Conte Inglés",
-  order: 1
-}
-console.log(nuevaSection)
+const sections = [
+  {
+    section: "Últimos Posts",
+    order: 1
+  },
+  {
+    section: "Guías",
+    order: 2
+  },
+  {
+    section: "Tips",
+    order: 3
+  },
+  {
+    section: "Sección sin contenido",
+    order: 4
+  }
+]
+
 
 sequelize.sync().then(() => {
   // console.log('Tablas creadas');
   // Insertamos datos:
-  Post.create(nuevoPost)
-  Section.create(nuevaSection)
+  
+  // Post.bulkCreate(sections)
+  // Section.bulkCreate(sections)
+
 }).catch((error) => {
   console.error('Hubo un error: ', error);
 });
